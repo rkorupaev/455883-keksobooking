c0ee86d1dc2284b1737544bb72256bc4222a3ad6
rkorupaev <rkorupaev@gmail.com>
2018-11-25 21:15:31 +0300

    pins on the map

--- js/map.js
+++ js/map.js
@@ -3,6 +3,7 @@ var featuresArray = ['wifi', 'dishwasher', 'parking'];
 var photosArray = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
+var pins = [];
 
 for (var i = 0; i < 8; i++) {
   anouncments[i] = {
@@ -29,7 +30,22 @@ for (var i = 0; i < 8; i++) {
       y: 250
     }
   }
-
-  console.log(anouncments[i]);
-
 }
+
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+// console.log(pinTemplate);
+var pin = pinTemplate.cloneNode(true);
+var pin2 = pinTemplate.cloneNode(true);
+var pin3 = pinTemplate.cloneNode(true);
+pin2.style = 'left: ' + anouncments[1].location.x + 'px; top: ' + anouncments[1].location.y + 'px;';
+pin3.style = 'left: 100px; top: 100px;';
+pin2.querySelector('img').src = anouncments[4].author.avatar;
+pin2.querySelector('img').alt = anouncments[4].offer.title;
+
+console.log(pin2);
+console.log(pin);
+
+var pinList = document.querySelector('.map__pins');
+
+pinList.appendChild(pin2);
+pinList.appendChild(pin3);
