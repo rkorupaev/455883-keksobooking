52722eac2e663e3f0ab71ad445033975293eba69
rkorupaev <rkorupaev@gmail.com>
2018-12-12 22:16:48 +0300

    eslint map.js fix

--- js/load.js
+++ js/load.js
@@ -1,17 +1,17 @@
-  'use strict';
+'use strict';
 
-  (function() {
-    var URL = 'https://js.dump.academy/keksobooking/data';
+(function () {
+  var URL = 'https://js.dump.academy/keksobooking/data';
 
-    window.load = function(onSuccess, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-      xhr.open('GET', URL);
+  window.load = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('GET', URL);
 
-      xhr.addEventListener('load', function() {
-        onSuccess(xhr.response);
-      });
+    xhr.addEventListener('load', function () {
+      onSuccess(xhr.response);
+    });
 
-      xhr.send();
-    };
-  })();
+    xhr.send();
+  };
+})();
