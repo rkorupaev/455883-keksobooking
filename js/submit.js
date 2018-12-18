b5bb0bb773acb398fceafcf06bb4e4edfeb723ca
rkorupaev <rkorupaev@gmail.com>
2018-12-18 21:58:47 +0300

    Revert "fix"
    This reverts commit fe08e5f09544817c38f34fe4ced30518639c23ab.

--- js/submit.js
+++ js/submit.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(function () {
+(function() {
   var form = document.querySelector('.ad-form');
   var main = document.querySelector('main');
   var succesMessageWindowTemplate = document.querySelector('#success').content.querySelector('.success');
@@ -9,30 +9,27 @@
   var errorMessageWindow = errorMessageWindowTemplate.cloneNode(true);
   var resetButton = form.querySelector('.ad-form__reset');
 
-  form.addEventListener('submit', function (evt) {
+  form.addEventListener('submit', function(evt) {
     evt.preventDefault();
     if (form.reportValidity() === true) {
-      window.backend.upload(new FormData(form), function () {
-        main.appendChild(succesMessageWindow);
-        window.util.resetPage();
-      }, function () {
+      window.backend.upload(new FormData(form), window.util.onSuccessHandler(main, succesMessageWindow), function() {
         main.appendChild(errorMessageWindow);
         errorMessageWindow.querySelector('.error__button').autofocus = true;
       });
     }
   });
 
-  succesMessageWindow.addEventListener('click', function () {
+  succesMessageWindow.addEventListener('click', function() {
     succesMessageWindow.parentNode.removeChild(succesMessageWindow);
   });
 
-  errorMessageWindow.addEventListener('click', function () {
+  errorMessageWindow.addEventListener('click', function() {
     errorMessageWindow.parentNode.removeChild(errorMessageWindow);
   });
   window.util.onEscKeydownHandler(succesMessageWindow);
   window.util.onEscKeydownHandler(errorMessageWindow);
 
-  resetButton.addEventListener('click', function () {
+  resetButton.addEventListener('click', function() {
     window.util.resetPage();
   });
 })();
