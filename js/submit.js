9e05a26596b0e9ea4cfe39c509d8a05651ffeecf
rkorupaev <rkorupaev@gmail.com>
2018-12-18 16:21:45 +0300

    eslint fix

--- js/submit.js
+++ js/submit.js
@@ -1,8 +1,7 @@
 'use strict';
 
-(function() {
+(function () {
   var form = document.querySelector('.ad-form');
-  var submitButton = form.querySelector('.ad-form__submit');
   var main = document.querySelector('main');
   var succesMessageWindowTemplate = document.querySelector('#success').content.querySelector('.success');
   var succesMessageWindow = succesMessageWindowTemplate.cloneNode(true);
@@ -10,37 +9,30 @@
   var errorMessageWindow = errorMessageWindowTemplate.cloneNode(true);
   var resetButton = form.querySelector('.ad-form__reset');
 
-  form.addEventListener('submit', function(evt) {
+  form.addEventListener('submit', function (evt) {
     evt.preventDefault();
     if (form.reportValidity() === true) {
-      window.upload(new FormData(form), function() {
+      window.upload(new FormData(form), function () {
         main.appendChild(succesMessageWindow);
         window.util.resetPage();
-      }, function() {
-          main.appendChild(errorMessageWindow);
-          errorMessageWindow.querySelector('.error__button').autofocus = true;
+      }, function () {
+        main.appendChild(errorMessageWindow);
+        errorMessageWindow.querySelector('.error__button').autofocus = true;
       });
     }
   });
 
-  // submitButton.addEventListener('click', function() {
-  //   if (form.reportValidity() === false) {
-  //     main.appendChild(errorMessageWindow);
-  //     errorMessageWindow.querySelector('.error__button').autofocus = true;
-  //   }
-  // });
-
-  succesMessageWindow.addEventListener('click', function() {
+  succesMessageWindow.addEventListener('click', function () {
     succesMessageWindow.parentNode.removeChild(succesMessageWindow);
   });
 
-  errorMessageWindow.addEventListener('click', function() {
+  errorMessageWindow.addEventListener('click', function () {
     errorMessageWindow.parentNode.removeChild(errorMessageWindow);
   });
   window.util.onEscKeydownHandler(succesMessageWindow);
   window.util.onEscKeydownHandler(errorMessageWindow);
 
-  resetButton.addEventListener('click', function() {
+  resetButton.addEventListener('click', function () {
     window.util.resetPage();
   });
 })();
--- js/upload.js
+++ js/upload.js
@@ -1,13 +1,13 @@
 'use strict';
 
-(function() {
+(function () {
   var URL = 'https://js.dump.academy/keksobooking';
 
-  window.upload = function(data, onSuccess, onError) {
+  window.upload = function (data, onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
-    xhr.addEventListener('load', function() {
+    xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
         onSuccess(xhr.response);
       } else {
